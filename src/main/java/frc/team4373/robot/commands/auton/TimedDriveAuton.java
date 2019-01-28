9a02dde2592c93a31d51f84217fe1d1960e647f9
Sam <samgauck@icloud.com>
2019-01-28 17:45:37 -0500

    Add TimedDriveAuton

--- src/main/java/frc/team4373/robot/commands/auton/TimedDriveAuton.java
+++ src/main/java/frc/team4373/robot/commands/auton/TimedDriveAuton.java
@@ -1,9 +1,43 @@
 package frc.team4373.robot.commands.auton;
 
+import edu.wpi.first.wpilibj.command.Command;
+import frc.team4373.robot.subsystems.Drivetrain;
+
 /**
  * A Javadoc template. TODO: Update TimedDriveAuton Javadoc.
  *
  * @author Samasaur
  */
-public class TimedDriveAuton {
+public class TimedDriveAuton extends Command {
+    private Drivetrain drivetrain;
+    private double power;
+    public TimedDriveAuton(double power, double seconds) {
+        requires(this.drivetrain = Drivetrain.getInstance());
+        setTimeout(seconds);
+    }
+
+    @Override
+    protected void initialize() {
+
+    }
+
+    @Override
+    protected void execute() {
+        this.drivetrain.setSidesPercentOutput(power);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return this.isTimedOut();
+    }
+
+    @Override
+    protected void end() {
+        drivetrain.zeroMotors();
+    }
+
+    @Override
+    protected void interrupted() {
+        this.end();
+    }
 }
