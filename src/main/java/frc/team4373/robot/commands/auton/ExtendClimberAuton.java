d5aee4585029adcb12b78bba142b2e813a5c2d8c
Sam <samgauck@icloud.com>
2019-01-29 17:05:12 -0500

    Add ExtendClimberAuton

---
+++ src/main/java/frc/team4373/robot/commands/auton/ExtendClimberAuton.java
@@ -0,0 +1,43 @@
+package frc.team4373.robot.commands.auton;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import edu.wpi.first.wpilibj.command.Command;
+import frc.team4373.robot.subsystems.Climber;
+import frc.team4373.robot.subsystems.Drivetrain;
+
+/**
+ * Extends the climber and sets the neutral mode of the drivetrain to coast.
+ *
+ * @author Samasaur
+ */
+public class ExtendClimberAuton extends Command {
+    private Climber climber;
+    private Drivetrain drivetrain;
+
+    public ExtendClimberAuton() {
+        requires(this.climber = Climber.getInstance());
+        this.drivetrain = Drivetrain.getInstance();
+    }
+
+    @Override
+    protected void initialize() {
+        setTimeout(0.5);
+    }
+
+    @Override
+    protected void execute() {
+        climber.climb();
+        drivetrain.setNeutralMode(NeutralMode.Coast);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return this.isTimedOut();
+    }
+
+    @Override
+    protected void end() {
+        climber.climb();
+        drivetrain.setNeutralMode(NeutralMode.Coast);
+    }
+}
