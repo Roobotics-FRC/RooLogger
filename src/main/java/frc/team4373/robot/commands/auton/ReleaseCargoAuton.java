56ec987d7d29e9b9e858146e77a097cefd920722
Ben <34463636+Benji123abc@users.noreply.github.com>
2019-01-30 16:36:16 -0500

    Add Release and Collect Cargo autons

---
+++ src/main/java/frc/team4373/robot/commands/auton/ReleaseCargoAuton.java
@@ -0,0 +1,29 @@
+package frc.team4373.robot.commands.auton;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.team4373.robot.subsystems.Intake;
+
+public class ReleaseCargoAuton extends Command {
+
+    private Intake intake;
+
+    public ReleaseCargoAuton() {
+        requires(this.intake = Intake.getInstance());
+    }
+
+    @Override
+    protected void initialize() {
+        setTimeout(0.5);
+    }
+
+    @Override
+    protected void execute() {
+        this.intake.releaseCargo();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return this.isTimedOut();
+    }
+
+}
